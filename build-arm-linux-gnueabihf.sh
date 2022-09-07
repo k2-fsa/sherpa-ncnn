cad825d1764615c34747e8c3c873a28e0b1fb8f9
Fangjun Kuang <csukuangfj@gmail.com>
2022-09-07 20:15:00 +0800

    Add CI for arm

--- build-arm-linux-gnueabihf.sh
+++ build-arm-linux-gnueabihf.sh
@@ -2,8 +2,9 @@
 
 set -x
 
-mkdir -p build-arm-linux-gnueabihf
-cd build-arm-linux-gnueabihf
+dir=build-arm-linux-gnueabihf
+mkdir -p $dir
+cd $dir
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
 make VERBOSE=1 -j4
 make install
