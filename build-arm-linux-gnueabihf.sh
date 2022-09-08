73a681e23c2c230877cd31cbbea8c28dedde1a2d
Fangjun Kuang <csukuangfj@gmail.com>
2022-09-08 08:31:35 +0800

    Add CI for arm (#6)
    * Add CI for arm
    * Build release version by default

--- build-arm-linux-gnueabihf.sh
+++ build-arm-linux-gnueabihf.sh
@@ -2,8 +2,9 @@
 
 set -x
 
-mkdir -p build-arm-linux-gnueabihf
-cd build-arm-linux-gnueabihf
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
+dir=build-arm-linux-gnueabihf
+mkdir -p $dir
+cd $dir
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
 make VERBOSE=1 -j4
 make install
