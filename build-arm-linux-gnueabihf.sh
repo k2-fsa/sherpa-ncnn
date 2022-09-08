1bcdcf39e019558d49d34b760173caec7ab84c6d
Fangjun Kuang <csukuangfj@gmail.com>
2022-09-08 08:29:48 +0800

    Build release version by default

--- build-arm-linux-gnueabihf.sh
+++ build-arm-linux-gnueabihf.sh
@@ -5,6 +5,6 @@ set -x
 dir=build-arm-linux-gnueabihf
 mkdir -p $dir
 cd $dir
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
 make VERBOSE=1 -j4
 make install
