cfbb6b2bdf57491df797b5f6019cf0644e3ae6e0
Fangjun Kuang <csukuangfj@gmail.com>
2022-12-08 08:19:23 +0800

    small fixes

--- sherpa-ncnn/csrc/meta-data.h
+++ sherpa-ncnn/csrc/meta-data.h
@@ -26,23 +26,7 @@ namespace sherpa_ncnn {
 
 class MetaData : public ncnn::Layer {
  public:
-  int load_param(const ncnn::ParamDict &pd) override {
-    arg0 = pd.get(0, 0), arg1 = pd.get(1, 0), arg2 = pd.get(2, 0);
-    arg3 = pd.get(3, 0), arg4 = pd.get(4, 0), arg5 = pd.get(5, 0);
-    arg6 = pd.get(6, 0), arg7 = pd.get(7, 0), arg8 = pd.get(8, 0);
-    arg9 = pd.get(9, 0), arg10 = pd.get(10, 0), arg11 = pd.get(11, 0);
-    arg12 = pd.get(12, 0), arg13 = pd.get(13, 0), arg14 = pd.get(14, 0);
-    arg15 = pd.get(15, 0), arg16 = pd.get(16, 0), arg17 = pd.get(17, 0);
-    arg18 = pd.get(18, 0), arg19 = pd.get(19, 0), arg20 = pd.get(20, 0);
-    arg21 = pd.get(21, 0), arg22 = pd.get(22, 0), arg23 = pd.get(23, 0);
-
-    // The following 8 attributes are of type float
-    arg24 = pd.get(24, 0.f), arg25 = pd.get(25, 0.f), arg26 = pd.get(26, 0.f);
-    arg27 = pd.get(27, 0.f), arg28 = pd.get(28, 0.f), arg29 = pd.get(29, 0.f);
-    arg30 = pd.get(30, 0.f), arg31 = pd.get(31, 0.f);
-
-    return 0;
-  }
+  int load_param(const ncnn::ParamDict &pd) override;
 
   int32_t arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7;
   int32_t arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15;
@@ -50,8 +34,6 @@ class MetaData : public ncnn::Layer {
 
   float arg24, arg25, arg26, arg27, arg28, arg29, arg30, arg31;
 };
-
-ncnn::Layer *MetaDataCreator(void *userdata);
 
 /*
 In encoder.ncnn.param, you can use
