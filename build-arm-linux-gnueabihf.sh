0edf99c7a93fac03b3bb83139222646b99bf949a
Fangjun Kuang <csukuangfj@gmail.com>
2022-12-15 12:02:56 +0800

    more fixes

--- build-arm-linux-gnueabihf.sh
+++ build-arm-linux-gnueabihf.sh
@@ -5,6 +5,10 @@ set -x
 dir=build-arm-linux-gnueabihf
 mkdir -p $dir
 cd $dir
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake ..
+cmake \
+  -DCMAKE_INSTALL_PREFIX=./install \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake \
+  ..
 make VERBOSE=1 -j4
-make install
+make install/strip
