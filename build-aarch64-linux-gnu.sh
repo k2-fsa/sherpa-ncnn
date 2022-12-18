f1add11491eb29a318abd92ccc6434bb7866e0d7
Fangjun Kuang <csukuangfj@gmail.com>
2022-12-18 15:12:23 +0800

    Small fixes to the cross-compiling scripts (#38)

--- build-aarch64-linux-gnu.sh
+++ build-aarch64-linux-gnu.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if ! command -v aarch64-linux-gnu-gcc  &> /dev/null; then
+  echo "Please install a toolchain for cross-compiling."
+  echo "You can refer to: "
+  echo "  https://k2-fsa.github.io/sherpa/ncnn/install/aarch64-embedded-linux.html"
+  echo "for help."
+  exit 1
+fi
+
 set -x
 
 dir=build-aarch64-linux-gnu
