8003a1f87b56e31f09ad1ed97320846a08e06e71
Fangjun Kuang <csukuangfj@gmail.com>
2023-01-29 11:25:49 +0800

    small fixes

--- ios-swift/SherpaNcnn/SherpaNcnn/Model.swift
+++ ios-swift/SherpaNcnn/SherpaNcnn/Model.swift
@@ -1,22 +1,133 @@
-
 import Foundation
-/// https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/conv-emformer-transducer-models.html#csukuangfj-sherpa-ncnn-conv-emformer-transducer-2022-12-06-chinese-english
-func getMultiLingualModelConfig2022_12_06() -> SherpaNcnnModelConfig{
-    let encoderParam = Bundle.main.path(forResource: "encoder_jit_trace-pnnx.ncnn", ofType: "param")
-    let encoderBin = Bundle.main.path(forResource: "encoder_jit_trace-pnnx.ncnn", ofType: "bin")
-    let decoderParam = Bundle.main.path(forResource: "decoder_jit_trace-pnnx.ncnn", ofType: "param")
-    let decoderBin = Bundle.main.path(forResource: "decoder_jit_trace-pnnx.ncnn", ofType: "bin")
-    let joinerParam = Bundle.main.path(forResource: "joiner_jit_trace-pnnx.ncnn", ofType: "param")
-    let joinerBin = Bundle.main.path(forResource: "joiner_jit_trace-pnnx.ncnn", ofType: "bin")
-    let tokens = Bundle.main.path(forResource: "tokens", ofType: "txt")
 
-    return sherpaNcnnModelConfig(encoderParam: encoderParam!,
-                                 encoderBin: encoderBin!,
-                                 decoderParam: decoderParam!,
-                                 decoderBin: decoderBin!,
-                                 joinerParam: joinerParam!,
-                                 joinerBin: joinerBin!,
-                                 tokens: tokens!,
-                                 numThreads: 4
-    )
+func getResource(_ forResource: String, _ ofType: String) -> String {
+  let path = Bundle.main.path(forResource: forResource, ofType: ofType)
+  precondition(
+    path != nil,
+    "\(forResource).\(ofType) does not exist!\n" + "Remember to change \n"
+      + "  Build Phases -> Copy Bundle Resources\n" + "to add it!"
+  )
+  return path!
 }
+/// Please refer to
+/// https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/index.html
+/// to download pre-trained models
+
+/// csukuangfj/sherpa-ncnn-conv-emformer-transducer-2022-12-06 (Chinese + English)
+func getMultilingualModelConfig2022_12_06() -> SherpaNcnnModelConfig {
+  let encoderParam = getResource("encoder_jit_trace-pnnx.ncnn", "param")
+  let encoderBin = getResource("encoder_jit_trace-pnnx.ncnn", "bin")
+  let decoderParam = getResource("decoder_jit_trace-pnnx.ncnn", "param")
+  let decoderBin = getResource("decoder_jit_trace-pnnx.ncnn", "bin")
+  let joinerParam = getResource("joiner_jit_trace-pnnx.ncnn", "param")
+  let joinerBin = getResource("joiner_jit_trace-pnnx.ncnn", "bin")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaNcnnModelConfig(
+    encoderParam: encoderParam,
+    encoderBin: encoderBin,
+    decoderParam: decoderParam,
+    decoderBin: decoderBin,
+    joinerParam: joinerParam,
+    joinerBin: joinerBin,
+    tokens: tokens,
+    numThreads: 4
+  )
+}
+
+/// csukuangfj/sherpa-ncnn-conv-emformer-transducer-2022-12-06 (Chinese + English)
+func getMultilingualModelConfig2022_12_06_Int8() -> SherpaNcnnModelConfig {
+  let encoderParam = getResource("encoder_jit_trace-pnnx.ncnn.int8", "param")
+  let encoderBin = getResource("encoder_jit_trace-pnnx.ncnn.int8", "bin")
+  let decoderParam = getResource("decoder_jit_trace-pnnx.ncnn", "param")
+  let decoderBin = getResource("decoder_jit_trace-pnnx.ncnn", "bin")
+  let joinerParam = getResource("joiner_jit_trace-pnnx.ncnn.int8", "param")
+  let joinerBin = getResource("joiner_jit_trace-pnnx.ncnn.int8", "bin")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaNcnnModelConfig(
+    encoderParam: encoderParam,
+    encoderBin: encoderBin,
+    decoderParam: decoderParam,
+    decoderBin: decoderBin,
+    joinerParam: joinerParam,
+    joinerBin: joinerBin,
+    tokens: tokens,
+    numThreads: 4
+  )
+}
+
+/// marcoyang/sherpa-ncnn-conv-emformer-transducer-small-2023-01-09 (English)
+func getConvEmformerSmallEnglishModelConfig2023_01_09() -> SherpaNcnnModelConfig {
+  let encoderParam = getResource("encoder_jit_trace-pnnx.ncnn", "param")
+  let encoderBin = getResource("encoder_jit_trace-pnnx.ncnn", "bin")
+  let decoderParam = getResource("decoder_jit_trace-pnnx.ncnn", "param")
+  let decoderBin = getResource("decoder_jit_trace-pnnx.ncnn", "bin")
+  let joinerParam = getResource("joiner_jit_trace-pnnx.ncnn", "param")
+  let joinerBin = getResource("joiner_jit_trace-pnnx.ncnn", "bin")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaNcnnModelConfig(
+    encoderParam: encoderParam,
+    encoderBin: encoderBin,
+    decoderParam: decoderParam,
+    decoderBin: decoderBin,
+    joinerParam: joinerParam,
+    joinerBin: joinerBin,
+    tokens: tokens,
+    numThreads: 4
+  )
+}
+
+/// marcoyang/sherpa-ncnn-conv-emformer-transducer-small-2023-01-09 (English)
+func getConvEmformerSmallEnglishModelConfig2023_01_09_Int8() -> SherpaNcnnModelConfig {
+  let encoderParam = getResource("encoder_jit_trace-pnnx.ncnn.int8", "param")
+  let encoderBin = getResource("encoder_jit_trace-pnnx.ncnn.int8", "bin")
+  let decoderParam = getResource("decoder_jit_trace-pnnx.ncnn", "param")
+  let decoderBin = getResource("decoder_jit_trace-pnnx.ncnn", "bin")
+  let joinerParam = getResource("joiner_jit_trace-pnnx.ncnn.int8", "param")
+  let joinerBin = getResource("joiner_jit_trace-pnnx.ncnn.int8", "bin")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaNcnnModelConfig(
+    encoderParam: encoderParam,
+    encoderBin: encoderBin,
+    decoderParam: decoderParam,
+    decoderBin: decoderBin,
+    joinerParam: joinerParam,
+    joinerBin: joinerBin,
+    tokens: tokens,
+    numThreads: 4
+  )
+}
+/// marcoyang/sherpa-ncnn-conv-emformer-transducer-small-2023-01-09 (English)
+func getLstmTransducerEnglish_2022_09_05() -> SherpaNcnnModelConfig {
+  let encoderParam = getResource(
+    "encoder_jit_trace-v2-iter-468000-avg-16-pnnx.ncnn", "param")
+  let encoderBin = getResource(
+    "encoder_jit_trace-v2-iter-468000-avg-16-pnnx.ncnn", "bin")
+  let decoderParam = getResource(
+    "decoder_jit_trace-v2-iter-468000-avg-16-pnnx.ncnn", "param")
+  let decoderBin = getResource(
+    "decoder_jit_trace-v2-iter-468000-avg-16-pnnx.ncnn", "bin")
+  let joinerParam = getResource(
+    "joiner_jit_trace-v2-iter-468000-avg-16-pnnx.ncnn", "param")
+  let joinerBin = getResource(
+    "joiner_jit_trace-v2-iter-468000-avg-16-pnnx.ncnn", "bin")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaNcnnModelConfig(
+    encoderParam: encoderParam,
+    encoderBin: encoderBin,
+    decoderParam: decoderParam,
+    decoderBin: decoderBin,
+    joinerParam: joinerParam,
+    joinerBin: joinerBin,
+    tokens: tokens,
+    numThreads: 4
+  )
+}
+
+/// Please refer to
+/// https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/index.html
+/// to add more models if you need
--- ios-swift/SherpaNcnn/SherpaNcnn/ViewController.swift
+++ ios-swift/SherpaNcnn/SherpaNcnn/ViewController.swift
@@ -5,177 +5,190 @@
 //  Created by fangjun on 2023/1/28.
 //
 
+import AVFoundation
 import UIKit
 
-import AVFoundation
-
 extension AudioBuffer {
-    func array() -> [Float] {
-        return Array(UnsafeBufferPointer(self))
-    }
+  func array() -> [Float] {
+    return Array(UnsafeBufferPointer(self))
+  }
 }
 
 extension AVAudioPCMBuffer {
-    func array() -> [Float] {
-        return self.audioBufferList.pointee.mBuffers.array()
-    }
+  func array() -> [Float] {
+    return self.audioBufferList.pointee.mBuffers.array()
+  }
 }
 
 class ViewController: UIViewController {
-    @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var recordBtn: UIButton!
+  @IBOutlet weak var resultLabel: UILabel!
+  @IBOutlet weak var recordBtn: UIButton!
 
-    var audioEngine: AVAudioEngine? = nil
-    var recognizer: SherpaNcnnRecognizer! = nil
+  var audioEngine: AVAudioEngine? = nil
+  var recognizer: SherpaNcnnRecognizer! = nil
 
-    /// It saves the decoded results so far
-    var sentences: [String] = [] {
-        didSet {
-            updateLabel()
-        }
+  /// It saves the decoded results so far
+  var sentences: [String] = [] {
+    didSet {
+      updateLabel()
     }
-    var lastSentence: String = ""
-    let maxSentence: Int = 20
-    var results: String {
-        if sentences.isEmpty && lastSentence.isEmpty {
-            return ""
-        }
-        if sentences.isEmpty {
-            return "0: \(lastSentence.lowercased())"
-        }
+  }
+  var lastSentence: String = ""
+  let maxSentence: Int = 20
+  var results: String {
+    if sentences.isEmpty && lastSentence.isEmpty {
+      return ""
+    }
+    if sentences.isEmpty {
+      return "0: \(lastSentence.lowercased())"
+    }
 
-        let start = max(sentences.count - maxSentence, 0)
-        if lastSentence.isEmpty {
-            return sentences.enumerated().map {(index, s) in "\(index): \(s.lowercased())"}[start...].joined(separator: "\n")
+    let start = max(sentences.count - maxSentence, 0)
+    if lastSentence.isEmpty {
+      return sentences.enumerated().map { (index, s) in "\(index): \(s.lowercased())" }[start...]
+        .joined(separator: "\n")
+    } else {
+      return sentences.enumerated().map { (index, s) in "\(index): \(s.lowercased())" }[start...]
+        .joined(separator: "\n") + "\n\(sentences.count): \(lastSentence.lowercased())"
+    }
+  }
+
+  func updateLabel() {
+    DispatchQueue.main.async {
+      self.resultLabel.text = self.results
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+
+    resultLabel.text = "ASR with Next-gen Kaldi\n\nPress the Start button to run!"
+    recordBtn.setTitle("Start", for: .normal)
+    initRecognizer()
+    initRecorder()
+  }
+
+  @IBAction func onRecordBtnClick(_ sender: UIButton) {
+    if recordBtn.currentTitle == "Start" {
+      startRecorder()
+      recordBtn.setTitle("Stop", for: .normal)
+    } else {
+      stopRecorder()
+      recordBtn.setTitle("Start", for: .normal)
+    }
+  }
+
+  func initRecognizer() {
+    // Please select one model that is best suitable for you.
+    //
+    // You can also modify Model.swift to add new pre-trained models from
+    // https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/index.html
+
+    var modelConfig = getMultilingualModelConfig2022_12_06()
+    // var modelConfig = getMultilingualModelConfig2022_12_06_Int8()
+    // var modelConfig = getConvEmformerSmallEnglishModelConfig2023_01_09()
+    // var modelConfig = getConvEmformerSmallEnglishModelConfig2023_01_09_Int8()
+    // var modelConfig = getLstmTransducerEnglish_2022_09_05()
+
+    var decoderConfig = sherpaNcnnDecoderConfig(
+      decodingMethod: "modified_beam_search",
+      numActivePaths: 4,
+      enableEndpoint: true,
+      rule1MinTrailingSilence: 1.2,
+      rule2MinTrailingSilence: 2.4,
+      rule3MinUtteranceLength: 200)
+
+    recognizer = SherpaNcnnRecognizer(
+      modelConfig: &modelConfig,
+      decoderConfig: &decoderConfig)
+  }
+
+  func initRecorder() {
+    print("init recorder")
+    audioEngine = AVAudioEngine()
+    let inputNode = self.audioEngine?.inputNode
+    let bus = 0
+    let inputFormat = inputNode?.outputFormat(forBus: bus)
+    let outputFormat = AVAudioFormat(
+      commonFormat: .pcmFormatFloat32,
+      sampleRate: 16000, channels: 1,
+      interleaved: false)!
+
+    let converter = AVAudioConverter(from: inputFormat!, to: outputFormat)!
+
+    inputNode!.installTap(
+      onBus: bus,
+      bufferSize: 1024,
+      format: inputFormat
+    ) {
+      (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+      var newBufferAvailable = true
+
+      let inputCallback: AVAudioConverterInputBlock = {
+        inNumPackets, outStatus in
+        if newBufferAvailable {
+          outStatus.pointee = .haveData
+          newBufferAvailable = false
+
+          return buffer
         } else {
-            return sentences.enumerated().map {(index, s) in "\(index): \(s.lowercased())"}[start...].joined(separator: "\n") + "\n\(sentences.count): \(lastSentence.lowercased())"
+          outStatus.pointee = .noDataNow
+          return nil
         }
-    }
+      }
 
-    func updateLabel() {
-        DispatchQueue.main.async {
-            self.resultLabel.text = self.results
-        }
-    }
+      let convertedBuffer = AVAudioPCMBuffer(
+        pcmFormat: outputFormat,
+        frameCapacity:
+          AVAudioFrameCount(outputFormat.sampleRate)
+          * buffer.frameLength
+          / AVAudioFrameCount(buffer.format.sampleRate))!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      var error: NSError?
+      let _ = converter.convert(
+        to: convertedBuffer,
+        error: &error, withInputFrom: inputCallback)
 
-        resultLabel.text = "ASR with Next-gen Kaldi\n\nPress the Start button to run!"
-        recordBtn.setTitle("Start", for: .normal)
-        initRecognizer()
-        initRecorder()
-    }
+      // TODO(fangjun): Handle status != haveData
 
-    @IBAction func onRecordBtnClick(_ sender: UIButton) {
-        if recordBtn.currentTitle == "Start" {
-            startRecorder()
-            recordBtn.setTitle("Stop", for: .normal)
-        } else {
-            stopRecorder()
-            recordBtn.setTitle("Start", for: .normal)
-        }
-    }
+      let array = convertedBuffer.array()
+      if !array.isEmpty {
+        self.recognizer.acceptWaveform(samples: array)
+        self.recognizer.decode()
+        let isEndpoint = self.recognizer.isEndpoint()
+        let text = self.recognizer.getResult().text
 
-    func initRecognizer() {
-        var modelConfig = getMultiLingualModelConfig2022_12_06()
-
-        var decoderConfig = sherpaNcnnDecoderConfig(decodingMethod: "modified_beam_search",
-                                                    numActivePaths: 4,
-                                                    enableEndpoint: true,
-                                                    rule1MinTrailingSilence: 1.2,
-                                                    rule2MinTrailingSilence: 2.4,
-                                                    rule3MinUtteranceLength: 200)
-
-        recognizer = SherpaNcnnRecognizer(modelConfig: &modelConfig,
-                                          decoderConfig: &decoderConfig)
-    }
-
-    func initRecorder() {
-        print("init recorder")
-        audioEngine = AVAudioEngine()
-        let inputNode = self.audioEngine?.inputNode
-        let bus = 0
-        let inputFormat = inputNode?.outputFormat(forBus: bus)
-        let outputFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32,
-                                         sampleRate: 16000, channels: 1,
-                                         interleaved: false)!
-
-        let converter = AVAudioConverter(from: inputFormat!, to: outputFormat)!
-
-        inputNode!.installTap(onBus: bus,
-                              bufferSize: 1024,
-                              format: inputFormat) {
-            (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
-            var newBufferAvailable = true
-
-            let inputCallback: AVAudioConverterInputBlock = {
-                inNumPackets, outStatus in
-                if newBufferAvailable {
-                    outStatus.pointee = .haveData
-                    newBufferAvailable = false
-
-                    return buffer
-                } else {
-                    outStatus.pointee = .noDataNow
-                    return nil
-                }
-            }
-
-            let convertedBuffer = AVAudioPCMBuffer(
-                pcmFormat: outputFormat,
-                frameCapacity:
-                    AVAudioFrameCount(outputFormat.sampleRate)
-                * buffer.frameLength
-                / AVAudioFrameCount(buffer.format.sampleRate))!
-
-            var error: NSError?
-            let _ = converter.convert(
-                to: convertedBuffer,
-                error: &error, withInputFrom: inputCallback)
-
-            // TODO(fangjun): Handle status != haveData
-
-
-            let array = convertedBuffer.array()
-            if !array.isEmpty {
-                self.recognizer.acceptWaveform(samples: array)
-                self.recognizer.decode()
-                let isEndpoint = self.recognizer.isEndpoint()
-                let text = self.recognizer.getResult().text
-
-                if !text.isEmpty && self.lastSentence != text {
-                    self.lastSentence = text
-                    self.updateLabel()
-                    print(text)
-                }
-
-                if isEndpoint && !text.isEmpty {
-                    let tmp = self.lastSentence
-                    self.lastSentence = ""
-                    self.sentences.append(tmp)
-                }
-            }
+        if !text.isEmpty && self.lastSentence != text {
+          self.lastSentence = text
+          self.updateLabel()
+          print(text)
         }
 
-    }
-
-    func startRecorder() {
-        lastSentence = ""
-        sentences = []
-
-        do {
-            try self.audioEngine?.start()
-        } catch let error as NSError {
-            print("Got an error starting audioEngine: \(error.domain), \(error)")
+        if isEndpoint && !text.isEmpty {
+          let tmp = self.lastSentence
+          self.lastSentence = ""
+          self.sentences.append(tmp)
         }
-        print("started")
+      }
     }
 
-    func stopRecorder() {
-        audioEngine?.stop()
-        print("stopped")
+  }
+
+  func startRecorder() {
+    lastSentence = ""
+    sentences = []
+
+    do {
+      try self.audioEngine?.start()
+    } catch let error as NSError {
+      print("Got an error starting audioEngine: \(error.domain), \(error)")
     }
+    print("started")
+  }
+
+  func stopRecorder() {
+    audioEngine?.stop()
+    print("stopped")
+  }
 }
-
