635cbf6a684ec18a44d0f5d405a23c99705d2b90
Fangjun Kuang <csukuangfj@gmail.com>
2023-05-03 01:07:47 +0800

    Add dotnet test

--- dotnet-examples/decode-file/run.sh
+++ dotnet-examples/decode-file/run.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-dotnet build -c Release
+dotnet build -c Release --verbosity normal
 
 # Please refer to
 # https://k2-fsa.github.io/sherpa/ncnn/pretrained_models/zipformer-transucer-models.html#csukuangfj-sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13-bilingual-chinese-english
 # to download the model files
 
-./bin/Release/net7.0/decode-file \
+if [ ! -d ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13 ]; then
+  GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/marcoyang/sherpa-ncnn-streaming-zipformer-zh-14M-2023-02-23
+  cd sherpa-ncnn-streaming-zipformer-zh-14M-2023-02-23
+  git lfs pull --include "*.bin"
+  cd ..
+fi
+
+dotnet run -c Release \
   ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/tokens.txt \
   ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/encoder_jit_trace-pnnx.ncnn.param \
   ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/encoder_jit_trace-pnnx.ncnn.bin \
@@ -14,5 +21,6 @@ dotnet build -c Release
   ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/decoder_jit_trace-pnnx.ncnn.bin \
   ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/joiner_jit_trace-pnnx.ncnn.param \
   ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/joiner_jit_trace-pnnx.ncnn.bin \
-  ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/test_wavs/1.wav
-
+  ./sherpa-ncnn-streaming-zipformer-bilingual-zh-en-2023-02-13/test_wavs/0.wav \
+  2 \
+  modified_beam_search
