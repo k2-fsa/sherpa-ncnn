45a1a9f3c187aa3cd53828483699dfffb371373b
Fangjun Kuang <csukuangfj@gmail.com>
2023-08-03 19:17:10 +0800

    add macos and windows

--- scripts/go/release.sh
+++ scripts/go/release.sh
@@ -62,39 +62,37 @@ git push origin $new_tag || true
 popd
 echo "========================================================================="
 
-exit 0
-
-git clone git@github.com:k2-fsa/sherpa-onnx-go-macos.git
+git clone git@github.com:k2-fsa/sherpa-ncnn-go-macos.git
 
 echo "Copy libs for macOS x86_64"
-rm -rf sherpa-onnx-go-macos/lib/x86_64-apple-darwin/lib*
-cp -v ./macos-x86_64/libkaldi-native-fbank-core.dylib sherpa-onnx-go-macos/lib/x86_64-apple-darwin
-cp -v ./macos-x86_64/libonnxruntime* sherpa-onnx-go-macos/lib/x86_64-apple-darwin
-cp -v ./macos-x86_64/libsherpa-onnx-c-api.dylib sherpa-onnx-go-macos/lib/x86_64-apple-darwin
-cp -v ./macos-x86_64/libsherpa-onnx-core.dylib sherpa-onnx-go-macos/lib/x86_64-apple-darwin
+rm -rf sherpa-ncnn-go-macos/lib/x86_64-apple-darwin/lib*
+cp -v ./macos-x86_64/libkaldi-native-fbank-core.dylib sherpa-ncnn-go-macos/lib/x86_64-apple-darwin
+cp -v ./macos-x86_64/libncnn.dylib sherpa-ncnn-go-macos/lib/x86_64-apple-darwin
+cp -v ./macos-x86_64/libsherpa-ncnn-c-api.dylib sherpa-ncnn-go-macos/lib/x86_64-apple-darwin
+cp -v ./macos-x86_64/libsherpa-ncnn-core.dylib sherpa-ncnn-go-macos/lib/x86_64-apple-darwin
 
 echo "Copy libs for macOS arm64"
-rm -rf sherpa-onnx-go-macos/lib/aarch64-apple-darwin/lib*
-cp -v ./macos-arm64/libkaldi-native-fbank-core.dylib sherpa-onnx-go-macos/lib/aarch64-apple-darwin
-cp -v ./macos-arm64/libonnxruntime* sherpa-onnx-go-macos/lib/aarch64-apple-darwin
-cp -v ./macos-arm64/libsherpa-onnx-c-api.dylib sherpa-onnx-go-macos/lib/aarch64-apple-darwin
-cp -v ./macos-arm64/libsherpa-onnx-core.dylib sherpa-onnx-go-macos/lib/aarch64-apple-darwin
+rm -rf sherpa-ncnn-go-macos/lib/aarch64-apple-darwin/lib*
+cp -v ./macos-arm64/libkaldi-native-fbank-core.dylib sherpa-ncnn-go-macos/lib/aarch64-apple-darwin
+cp -v ./macos-arm64/libncnn.dylib sherpa-ncnn-go-macos/lib/aarch64-apple-darwin
+cp -v ./macos-arm64/libsherpa-ncnn-c-api.dylib sherpa-ncnn-go-macos/lib/aarch64-apple-darwin
+cp -v ./macos-arm64/libsherpa-ncnn-core.dylib sherpa-ncnn-go-macos/lib/aarch64-apple-darwin
 
 echo "Copy sources for macOS"
-cp sherpa-onnx/c-api/c-api.h sherpa-onnx-go-macos/
-cp scripts/go/sherpa_onnx.go sherpa-onnx-go-macos/
+cp sherpa-ncnn/c-api/c-api.h sherpa-ncnn-go-macos/
+cp scripts/go/sherpa_ncnn.go sherpa-ncnn-go-macos/
 
-pushd sherpa-onnx-go-macos
+pushd sherpa-ncnn-go-macos
 tag=$(git describe --abbrev=0 --tags)
 if [[ x"$VERSION" == x"auto" ]]; then
   # this is a pre-release
-  if [[ $tag == ${SHERPA_ONNX_VERSION}* ]]; then
+  if [[ $tag == ${SHERPA_NCNN_VERSION}* ]]; then
     # echo we have already release pre-release before, so just increment it
     last=$(echo $tag | rev | cut -d'.' -f 1 | rev)
     new_last=$((last+1))
-    new_tag=${SHERPA_ONNX_VERSION}-alpha.${new_last}
+    new_tag=${SHERPA_NCNN_VERSION}-alpha.${new_last}
   else
-    new_tag=${SHERPA_ONNX_VERSION}-alpha.1
+    new_tag=${SHERPA_NCNN_VERSION}-alpha.1
   fi
 else
   new_tag=$VERSION
@@ -111,36 +109,36 @@ git push origin $new_tag || true
 popd
 echo "========================================================================="
 
-git clone git@github.com:k2-fsa/sherpa-onnx-go-windows.git
+git clone git@github.com:k2-fsa/sherpa-ncnn-go-windows.git
 echo "Copy libs for Windows x86_64"
-rm -fv sherpa-onnx-go-windows/lib/x86_64-pc-windows-gnu/*
-cp -v ./windows-x64/kaldi-native-fbank-core.dll sherpa-onnx-go-windows/lib/x86_64-pc-windows-gnu
-cp -v ./windows-x64/onnxruntime.dll sherpa-onnx-go-windows/lib/x86_64-pc-windows-gnu
-cp -v ./windows-x64/sherpa-onnx-c-api.dll sherpa-onnx-go-windows/lib/x86_64-pc-windows-gnu
-cp -v ./windows-x64/sherpa-onnx-core.dll sherpa-onnx-go-windows/lib/x86_64-pc-windows-gnu
+rm -fv sherpa-ncnn-go-windows/lib/x86_64-pc-windows-gnu/*
+cp -v ./windows-x64/kaldi-native-fbank-core.lib sherpa-ncnn-go-windows/lib/x86_64-pc-windows-gnu
+cp -v ./windows-x64/ncnn.lib sherpa-ncnn-go-windows/lib/x86_64-pc-windows-gnu
+cp -v ./windows-x64/sherpa-ncnn-c-api.lib sherpa-ncnn-go-windows/lib/x86_64-pc-windows-gnu
+cp -v ./windows-x64/sherpa-ncnn-core.lib sherpa-ncnn-go-windows/lib/x86_64-pc-windows-gnu
 
 echo "Copy libs for Windows x86"
-rm -fv sherpa-onnx-go-windows/lib/i686-pc-windows-gnu/*
-cp -v ./windows-win32/kaldi-native-fbank-core.dll sherpa-onnx-go-windows/lib/i686-pc-windows-gnu
-cp -v ./windows-win32/onnxruntime.dll sherpa-onnx-go-windows/lib/i686-pc-windows-gnu
-cp -v ./windows-win32/sherpa-onnx-c-api.dll sherpa-onnx-go-windows/lib/i686-pc-windows-gnu
-cp -v ./windows-win32/sherpa-onnx-core.dll sherpa-onnx-go-windows/lib/i686-pc-windows-gnu
+rm -fv sherpa-ncnn-go-windows/lib/i686-pc-windows-gnu/*
+cp -v ./windows-win32/kaldi-native-fbank-core.lib sherpa-ncnn-go-windows/lib/i686-pc-windows-gnu
+cp -v ./windows-win32/ncnn.lib sherpa-ncnn-go-windows/lib/i686-pc-windows-gnu
+cp -v ./windows-win32/sherpa-ncnn-c-api.lib sherpa-ncnn-go-windows/lib/i686-pc-windows-gnu
+cp -v ./windows-win32/sherpa-ncnn-core.lib sherpa-ncnn-go-windows/lib/i686-pc-windows-gnu
 
 echo "Copy sources for Windows"
-cp sherpa-onnx/c-api/c-api.h sherpa-onnx-go-windows/
-cp scripts/go/sherpa_onnx.go sherpa-onnx-go-windows/
+cp sherpa-ncnn/c-api/c-api.h sherpa-ncnn-go-windows/
+cp scripts/go/sherpa_ncnn.go sherpa-ncnn-go-windows/
 
-pushd sherpa-onnx-go-windows
+pushd sherpa-ncnn-go-windows
 tag=$(git describe --abbrev=0 --tags)
 if [[ x"$VERSION" == x"auto" ]]; then
   # this is a pre-release
-  if [[ $tag == ${SHERPA_ONNX_VERSION}* ]]; then
+  if [[ $tag == ${SHERPA_NCNN_VERSION}* ]]; then
     # echo we have already release pre-release before, so just increment it
     last=$(echo $tag | rev | cut -d'.' -f 1 | rev)
     new_last=$((last+1))
-    new_tag=${SHERPA_ONNX_VERSION}-alpha.${new_last}
+    new_tag=${SHERPA_NCNN_VERSION}-alpha.${new_last}
   else
-    new_tag=${SHERPA_ONNX_VERSION}-alpha.1
+    new_tag=${SHERPA_NCNN_VERSION}-alpha.1
   fi
 else
   new_tag=$VERSION
