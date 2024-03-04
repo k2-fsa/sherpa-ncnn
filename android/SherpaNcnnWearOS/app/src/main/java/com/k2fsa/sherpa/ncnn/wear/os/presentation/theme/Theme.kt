7ad95d71f876100bc1f883d9aece9ec1a504bd48
Fangjun Kuang <csukuangfj@gmail.com>
2024-03-04 12:22:28 +0800

    Initial commit after creating the project

---
+++ android/SherpaNcnnWearOS/app/src/main/java/com/k2fsa/sherpa/ncnn/wear/os/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.k2fsa.sherpa.ncnn.wear.os.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun SherpaNcnnWearOSTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
