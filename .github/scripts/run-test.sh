fdebe225451e21b8323a53f7f95cb2c8a034e2c9
pkufool <wkang.pku@gmail.com>
2024-03-07 11:46:58 +0800

    add ci test for hotwords

--- .github/scripts/run-test.sh
+++ .github/scripts/run-test.sh
@@ -52,6 +52,24 @@ for wave in ${waves[@]}; do
   done
 done
 
+log "-----------------------------------------------------------------"
+log "Run Zipformer transducer (Chinese, small model 14M) for hotwords "
+log "-----------------------------------------------------------------"
+log "Start testing ${repo_url} with hotwords"
+
+time $EXE \
+  $repo/tokens.txt \
+  $repo/encoder_jit_trace-pnnx.ncnn.param \
+  $repo/encoder_jit_trace-pnnx.ncnn.bin \
+  $repo/decoder_jit_trace-pnnx.ncnn.param \
+  $repo/decoder_jit_trace-pnnx.ncnn.bin \
+  $repo/joiner_jit_trace-pnnx.ncnn.param \
+  $repo/joiner_jit_trace-pnnx.ncnn.bin \
+  $repo/test_wavs/1.wav \
+  2 \
+  modified_beam_search \
+  $repo/test_wavs/hotwords.txt
+
 rm -rf $repo
 
 log "------------------------------------------------------------"
