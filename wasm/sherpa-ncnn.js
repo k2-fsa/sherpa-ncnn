428400af76f855881c769cc628ea9892ea8b3033
chen-junqiao <69945591+chen-junqiao@users.noreply.github.com>
2025-04-15 18:09:09 +0800

    Update wasm/sherpa-ncnn.js
    Co-authored-by: Fangjun Kuang <csukuangfj@gmail.com>

--- wasm/sherpa-ncnn.js
+++ wasm/sherpa-ncnn.js
@@ -144,7 +144,7 @@ function initSherpaNcnnRecognizerConfig(config, Module) {
   Module.setValue(ptr + offset, buffer, 'i8*');  // hotwords file
   offset += 4;
 
-  Module.setValue(ptr + offset, config.decoderConfig.hotwordsScore || 0.5, 'float');  // hotwords_score
+  Module.setValue(ptr + offset, config.hotwordsScore || 0.5, 'float');  // hotwords_score
   offset += 4;
 
   return {
